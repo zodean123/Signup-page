ffa288d099e3655837598dcc332116bf40b3e6f7
zodean123 <dipanmallick7085@gmail.com>
2022-08-27 01:23:08 +0530

    first commit

---
+++ src/containers/Detailspage.svelte
@@ -0,0 +1,52 @@
+<script>
+      import { DataTable, Button } from "carbon-components-svelte";
+      function clear(){
+    window.localStorage.clear();
+    window.reload();
+  }
+      
+</script>
+<div class="box">
+  <h1>THANK U FOR LOGGING US....<br>Here are the details!!!!</h1>
+
+<DataTable
+  headers={[
+    { key: "name", value: "name" },
+    { key: "email", value: "email" },
+    { key: "mobile", value: "mobile" },
+    { key: "profession", value: "profession" },
+    { key: "password", value: "password" },
+  ]} 
+  rows = {
+   JSON.parse(window.localStorage.getItem("signupinfo"))
+  }
+  >
+ 
+</DataTable>
+<div class = "space">
+<Button expressive kind="danger" on:click = {clear}>Logout</Button>
+</div>
+</div>
+<style>
+    .box{
+        box-sizing: border-box;
+        box-shadow: 10px 10px rgb(79, 247, 239);
+        width:80vw;
+        height:auto;
+        position: fixed;
+margin-top:100px;
+margin-left: 180px;
+    }
+    .space{
+        padding:20px;
+        justify-content: center;
+        display: flex;
+    }
+    h1{
+      color:crimson;
+      font-weight: 900;
+      display: flex;
+    justify-content: center;
+    padding: 30px;
+    }
+</style>
---
+++ src/containers/LoginPage.svelte
@@ -0,0 +1,68 @@
+<script>
+	let validatename,validatepassword;
+ import {
+    Form,
+    FormGroup,
+    TextInput,
+    PasswordInput,
+Button,
+  } from "carbon-components-svelte";
+import { navigate } from "svelte-routing";
+
+
+
+ function showdetails(){
+  
+  let getobj =window.localStorage.getItem("signupinfo")||"[]";
+  let getobject = JSON.parse(getobj);
+
+for(var i=0; i<getobj.length;i++){
+ const name= getobject[i]?.name;
+const getpassword = getobject[i]?.password;
+  if((name == validatename) && (getpassword == validatepassword)){
+    navigate("/details");
+  }
+  else{
+console.log("Invalid message");
+  }
+  
+}
+ }
+  
+</script>
+<h1>Welcome to Signin page!!!</h1>
+<div class = "box">
+<Form>
+    <FormGroup>
+    <TextInput  bind:value = {validatename} labelText="User name" placeholder="User name..." required />
+
+<PasswordInput bind:value = {validatepassword}
+  required
+  type="password"
+  labelText="Password"
+  placeholder="password..."
+/>
+<Button expressive kind="danger"  on:click = {showdetails}>Next</Button>
+   </FormGroup>
+</Form>
+</div>
+<style>
+    .box{
+box-sizing: border-box;
+padding: 20px;
+border-radius: 20px;
+background-color: rgb(205, 244, 244);
+margin-top:10rem;
+margin-left: 30rem;
+width:40vw;
+    }
+    h1{
+      font-size: 5rem;
+      color:rgb(246, 119, 253);
+      font-weight: 1000;
+      display: flex;
+      justify-content: center;
+      position: relative;
+      top:50px;
+    }
+</style>
